6035a8eb74586df9917e90889776b2086afc62fa
Aasim <syedaasim133@gmail.com>
2022-01-28 19:59:06 +0530

    Updates

---
+++ precache-manifest.d4f42070cf78bdbb7f1ba43cf93a9c1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "344b9ea88979504862f8265ebb697870",
+    "url": "/carrermp3/index.html"
+  },
+  {
+    "revision": "4eb98ae811e115868999",
+    "url": "/carrermp3/static/css/main.540d8454.chunk.css"
+  },
+  {
+    "revision": "17870537760683f7a5bb",
+    "url": "/carrermp3/static/js/2.082c3e78.chunk.js"
+  },
+  {
+    "revision": "eff686c24e57c5260f9f",
+    "url": "/carrermp3/static/js/3.6e32c79a.chunk.js"
+  },
+  {
+    "revision": "4eb98ae811e115868999",
+    "url": "/carrermp3/static/js/main.64f1d857.chunk.js"
+  },
+  {
+    "revision": "b39e06738072f0c68835",
+    "url": "/carrermp3/static/js/runtime~main.984f6f63.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/carrermp3/static/media/logo.06e73328.svg"
+  }
+]);
